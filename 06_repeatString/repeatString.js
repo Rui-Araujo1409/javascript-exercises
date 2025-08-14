0a20e1f4c3cbae3dd5ac2e0455818f402ac9cf69
Rui-Araujo1409 <rui1409@gmail.com>
2025-08-14 11:55:21 +0000

    TOP - exercícios JS

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+    let frase = "";
+    if (num < 0) {
+        frase= "ERROR";
+        return frase;
+    } else {
+        for (let i = 1; i <= num; i++) {
+            frase += string;
+        }
+        return frase;
+    }
 };
 
 // Do not edit below this line
--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+let array = string.split("");
+let resultado = array.reverse().join("");
+return resultado;
 };
 
 // Do not edit below this line
