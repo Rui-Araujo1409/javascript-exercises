5c62a115a7cb0d36849bf517b6036025dfb76120
Rui-Araujo1409 <rui1409@gmail.com>
2025-09-16 16:24:04 +0000

    exercício 13

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,28 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((total, valor) => total + valor, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+return array.reduce((total, valor) => total * valor, 1);
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return num1**num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	 let cont = 1;
+for(i=1; i<=num; i++) {
+  cont *=i;
+} return cont;
 };
 
 // Do not edit below this line
